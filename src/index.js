528fe2ebfc4d76fd332839c0a8c869667c6480b0
Hyung-Jung Son <thsgudwjd@nate.com>
2023-07-17 16:08:15 +0900

    dev 브랜치 - 초기세팅

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Reset } from 'styled-reset';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Reset />
       <App />
     </BrowserRouter>
   </React.StrictMode>
