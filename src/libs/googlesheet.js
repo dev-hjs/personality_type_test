348497b5dd6c08ad49412310002939d33f0abf26
Hyung-Jung Son <thsgudwjd@nate.com>
2023-07-19 15:17:19 +0900

    feature/question2 - 구글시트 api 연결 및 데이터 불러오기

---
+++ src/libs/googlesheet.js
@@ -0,0 +1,36 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { useEffect, useState } from 'react';
+import { JWT } from 'google-auth-library';
+
+const credential = require('../libs/typetest1-0282f9b10c39.json');
+
+export const getGoogleSheet = async ({ googleSheetRows }) => {
+  const doc = new GoogleSpreadsheet('1zcM_t4HRDzfI2WR5l6nkW9fEEPMK13xskvxV6gbjez8');
+  await doc.useServiceAccountAuth(credential);
+  await doc.loadInfo();
+  return doc;
+};
+
+export const useGoogleSheet = (sheetId) => {
+  const [googleSheetRows, setGoogleSheetRows] = useState([]);
+
+  // const fetchGoogleSheetRows = async () => {
+  //   const googleSheet = await getGoogleSheet();
+  //   const sheet = googleSheet.sheetsById[sheetId];
+  //   const rows = await sheet.getRows();
+  //   setGoogleSheetRows(rows);
+  // };
+
+  const fetchGoogleSheetRows = async () => {
+    const googleSheet = await getGoogleSheet();
+    const sheetsByIdElement = googleSheet.sheetsById[sheetId];
+    // 행들을 가져옵니다.
+    const rows = await sheetsByIdElement.getRows();
+    setGoogleSheetRows(rows);
+  };
+
+  useEffect(() => {
+    fetchGoogleSheetRows();
+  }, []);
+  return [googleSheetRows];
+};
--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -1,113 +1,57 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Question from './contents/Question';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-const QuestionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  margin-bottom: 20px;
-`;
+const credential = require('../libs/typetest1-0282f9b10c39.json');
 
-const QuestionBox = styled.label`
-  width: 700px;
-  height: 100px;
-  border: 1px solid #000;
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-  cursor: pointer;
-`;
+// Google 스프레드시트를 가져오는 함수
+export const getGoogleSheet = async () => {
+  const doc = new GoogleSpreadsheet('1zcM_t4HRDzfI2WR5l6nkW9fEEPMK13xskvxV6gbjez8');
+  // Google 인증이 필요합니다.
+  await doc.useServiceAccountAuth(credential);
+  await doc.loadInfo();
+  return doc;
+};
 
-const CheckboxInput = styled.input`
-  margin-right: 10px;
-`;
+// Google 스프레드시트 행을 가져오는 커스텀 훅
+export const useGoogleSheet = (sheetId) => {
+  const [googleSheetRows, setGoogleSheetRows] = useState([]);
 
-const QuestionPage = () => {
-  const navigate = useNavigate();
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedOption, setSelectedOption] = useState('');
-  const youtubeVideoId = 'DwuJeGYlYyw';
-
-  const handleCheckboxChange = (option) => {
-    setSelectedOption(option);
+  const fetchGoogleSheetRows = async () => {
+    const googleSheet = await getGoogleSheet();
+    const sheetsByIdElement = googleSheet.sheetsById[sheetId];
+    // 행들을 가져옵니다.
+    const rows = await sheetsByIdElement.getRows();
+    setGoogleSheetRows(rows);
   };
 
-  const handleNextQuestion = () => {
-    if (selectedOption !== '') {
-      const answer = {
-        question: currentQuestionData.question,
-        answer: selectedOption,
-        name: currentQuestionData.answer.find((ans) => ans.text === selectedOption)?.name
-      };
-      saveAnswerToServer(answer);
+  useEffect(() => {
+    fetchGoogleSheetRows();
+  }, []);
 
-      if (currentQuestion < Question.length - 1) {
-        setCurrentQuestion(currentQuestion + 1);
-        setSelectedOption('');
-      } else {
-        navigate('/result');
-      }
-    } else {
-      alert('질문에 답변해주세요!');
-    }
-  };
+  return [googleSheetRows];
+};
 
-  const handlePreviousQuestion = () => {
-    if (currentQuestion > 0) {
-      setCurrentQuestion(currentQuestion - 1);
-      setSelectedOption('');
-    } else {
-      alert('첫번째 질문입니다!');
-    }
-  };
+function QuestionPage() {
+  const [data] = useGoogleSheet('714994551');
 
-  const currentQuestionData = Question[currentQuestion];
-
-  const iframeRef = useRef(null);
-
-  const saveAnswerToServer = (answer) => {
-    fetch('http://localhost:4000/questions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(answer)
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Answer saved to server:', data);
-      })
-      .catch((error) => {
-        console.error('Error saving answer to server:', error);
-      });
-  };
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <>
+      {data.map((row, index) => (
+        <div key={index}>
+          <div> {row._rawData[0]}</div>
+          <div> {row._rawData[1]}</div>
+          <div> {row._rawData[2]}</div>
+        </div>
+      ))}
       <div>QuestionPage</div>
-      <span>{currentQuestionData.question}</span>
-      <QuestionContainer>
-        {currentQuestionData.answer.map((ans, ansIndex) => (
-          <QuestionBox key={ansIndex}>
-            <CheckboxInput
-              type="checkbox"
-              checked={selectedOption === ans.text}
-              onChange={() => handleCheckboxChange(ans.text)}
-              id={`question${currentQuestion + 1}`}
-            />
-            <span>{ans.text}</span>
-          </QuestionBox>
-        ))}
-      </QuestionContainer>
-      <button onClick={handlePreviousQuestion}>이전 질문</button>
-      <button onClick={handleNextQuestion}>다음 질문</button>
-      <Link to="/result">
-        <button>결과 보러 가기</button>
-      </Link>
     </>
   );
-};
+}
 
 export default QuestionPage;
