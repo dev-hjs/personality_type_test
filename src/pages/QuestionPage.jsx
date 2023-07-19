b06eada7e9ab558c246a6e9fa82fb0a26fa68b60
Hyung-Jung Son <thsgudwjd@nate.com>
2023-07-19 16:58:04 +0900

    feature/question2 - 구글시트 api 연결 및 데이터 질문별로 답변리스트 보여주기

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
-
 const credential = require('../libs/typetest1-0282f9b10c39.json');
-
 // Google 스프레드시트를 가져오는 함수
 export const getGoogleSheet = async () => {
   const doc = new GoogleSpreadsheet('1zcM_t4HRDzfI2WR5l6nkW9fEEPMK13xskvxV6gbjez8');
@@ -13,11 +11,9 @@ export const getGoogleSheet = async () => {
   await doc.loadInfo();
   return doc;
 };
-
 // Google 스프레드시트 행을 가져오는 커스텀 훅
 export const useGoogleSheet = (sheetId) => {
   const [googleSheetRows, setGoogleSheetRows] = useState([]);
-
   const fetchGoogleSheetRows = async () => {
     const googleSheet = await getGoogleSheet();
     const sheetsByIdElement = googleSheet.sheetsById[sheetId];
@@ -25,33 +21,108 @@ export const useGoogleSheet = (sheetId) => {
     const rows = await sheetsByIdElement.getRows();
     setGoogleSheetRows(rows);
   };
-
   useEffect(() => {
     fetchGoogleSheetRows();
   }, []);
-
   return [googleSheetRows];
 };
-
 function QuestionPage() {
-  const [data] = useGoogleSheet('714994551');
-
+  const [data] = useGoogleSheet('16447129');
+  const [selectedCheckboxIndex, setSelectedCheckboxIndex] = useState(-1);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const navigate = useNavigate();
   useEffect(() => {
     console.log(data);
   }, [data]);
-
+  const handleAnswerSelection = (answerIndex) => {
+    setSelectedCheckboxIndex(answerIndex);
+  };
+  const handleNextQuestion = () => {
+    if (selectedCheckboxIndex !== -1) {
+      // 현재 질문의 답변 선택
+      // 다음 질문으로 이동
+      setSelectedCheckboxIndex(-1);
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+  const handlePrevQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+  const isPrevButtonVisible = currentQuestionIndex > 0;
   return (
     <>
-      {data.map((row, index) => (
-        <div key={index}>
-          <div> {row._rawData[0]}</div>
-          <div> {row._rawData[1]}</div>
-          <div> {row._rawData[2]}</div>
-        </div>
-      ))}
-      <div>QuestionPage</div>
+      {data.map(
+        (row, index) =>
+          // 현재 질문과 답변 리스트만 보여주도록 설정
+          index === currentQuestionIndex && (
+            <QuestionContainer key={index}>
+              <div> {row._rawData[0]}</div>
+              {row._rawData[1].split('\n').map((answer, answerIndex) => (
+                <QuestionBox key={answerIndex}>
+                  <CheckboxInput
+                    type="checkbox"
+                    checked={selectedCheckboxIndex === answerIndex}
+                    onChange={() => handleAnswerSelection(answerIndex)}
+                  />
+                  <span>{`${answerIndex + 1}: ${answer}`}</span>
+                </QuestionBox>
+              ))}
+            </QuestionContainer>
+          )
+      )}
+      <ButtonContainer>
+        {isPrevButtonVisible && (
+          <PrevButton onClick={handlePrevQuestion} disabled={currentQuestionIndex === 0}>
+            이전
+          </PrevButton>
+        )}
+        <NextButton onClick={handleNextQuestion} disabled={selectedCheckboxIndex === -1}>
+          다음
+        </NextButton>
+      </ButtonContainer>
     </>
   );
 }
-
 export default QuestionPage;
+const QuestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 20px;
+`;
+const QuestionBox = styled.label`
+  width: 700px;
+  height: 100px;
+  border: 1px solid #000;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  cursor: pointer;
+`;
+const CheckboxInput = styled.input`
+  margin-right: 10px;
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  margin-top: 20px;
+`;
+const PrevButton = styled.button`
+  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  background-color: ${({ disabled }) => (disabled ? '#ccc' : '#DC3545')};
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+`;
+const NextButton = styled.button`
+  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  background-color: ${({ disabled }) => (disabled ? '#ccc' : '#007BFF')};
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+`;
