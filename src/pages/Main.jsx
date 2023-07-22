f37725ac60f75f113c9d4a26b3edd1148ea3504e
Hyung-Jung Son <thsgudwjd@nate.com>
2023-07-23 03:43:02 +0900

    글리치 배포, locahost 주소 변경

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -14,14 +14,14 @@ function Main() {
     async function fetchData() {
       //axios를 이용해서 방문자 수 데이터 가져오기
       try {
-        const res = await axios.get('http://localhost:4000/visit');
+        const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/visit`);
         console.log('res!!', res.data);
 
         //가져온 방문자 수 저장
         const originalData = res.data.user;
 
         //방문자 수 증가시켜서 업데이트
-        axios.patch('http://localhost:4000/visit', {
+        axios.patch(`${process.env.REACT_APP_SERVER_URL}/visit`, {
           user: originalData + 1
         });
 
@@ -94,7 +94,7 @@ function Main() {
         </Link>
         <br></br>
         <CopyLinkIcon onClick={() => handleCopyClipBoard('http://localhost:3000/')}>
-          <img src="linkCopy.png" alt="LinkCopy" />
+          <img src="LinkCopy.png" alt="LinkCopy" />
         </CopyLinkIcon>
         <OtherLanguages>▼ OTHER LANGUAGES ▼</OtherLanguages>
         <StFlagimg>
